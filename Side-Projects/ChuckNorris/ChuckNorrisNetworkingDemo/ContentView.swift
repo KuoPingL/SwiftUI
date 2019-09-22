1d21ad82d2eb26ae198465e46a40cfda54619a32
KuoPingL <46020404+KuoPingL@users.noreply.github.com>
2019-09-22 23:39:04 +0800

    First Commit

---
+++ Side-Projects/ChuckNorris/ChuckNorrisNetworkingDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChuckNorrisNetworkingDemo
+//
+//  Created by Jimmy on 2019/9/22.
+//  Copyright © 2019 Jimmy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
